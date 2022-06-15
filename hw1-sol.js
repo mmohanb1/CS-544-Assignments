4cc6aeb4b6685f349e221d9e75ff1d114bc84874
Murali Mohan Bugata <mmohanb1@CS444-mmohanb1.bu.binghamton.edu>
2022-06-15 11:29:07 +0000

    6Functions

--- hw1-sol.js
+++ hw1-sol.js
@@ -18,8 +18,23 @@ function selector(obj, selector){
     const elems = Array.from(arr.slice(1, arr.length)).reduce((acc, e, i) => acc.concat(acc.at(-1)[e]) ,[obj[arr[0]]]);
     return elems[elems.length-1];
 }
+function lineAt(str, n)
+{
+  if(str.charAt(n) === '\n' || n < 0 || n >= str.length) return '';
+
+  const s1 = str.slice(0, n+1);
+  const s2 = s1.slice(s1.lastIndexOf('\n')+1, n+1);
+  const s3 = str.slice(n+1, str.length);
+  const s4 = s3.slice(0, s3.indexOf('\n') === -1 ? s3.length : s3.indexOf('\n'));
+
+  return s2.concat(s4);
+
+}
 /*console.log(rmPrefixSufix("twas brillig and the slithy toves",-1,-4));*/
 /*console.log(applyToEvens([1,2,3,4,5,6,7],n => n*2));*/
 /*console.log(productOdds([]));*/
 //console.log(strIndexesStr('Hello World',-1,-2));
-console.log(selector({a: [1, {b: 22}], c: 33, d: {f: {m: {s: {x: -32}}}}}, 'd.f.m.s.x'));
+//console.log(selector({a: [1, {b: 22}], c: 33, d: {f: {m: {s: {x: -32}}}}}, 'd.f.m.s.x'));
+const str = '012\nabcd\n87t8\n989'
+console.log(lineAt(str, 12));
+//console.log(selector({a: [1, {b: 22}], c: 33}, 'd'));
