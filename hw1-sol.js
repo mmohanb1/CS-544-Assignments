7e3daca83bc771187ab869807caf18ee5708841d
Murali Mohan Bugata <mmohanb1@CS444-mmohanb1.bu.binghamton.edu>
2022-06-15 07:43:26 +0000

    first4Functions

--- hw1-sol.js
+++ hw1-sol.js
@@ -6,5 +6,14 @@ function applyToEvens(list, func){
     const nArr = list.filter(a => a%2 == 0);
     return nArr.map(func);
 }
+function productOdds(list){
+    const p=1;
+    return list.filter(a => a%2 != 0).reduce((prev, curr) => prev*curr, p);
+}
+function strIndexesStr(s, ...indexes){
+    return Array.from(indexes, a => s[a]).toString().replaceAll(',','');
+}
 /*console.log(rmPrefixSufix("twas brillig and the slithy toves",-1,-4));*/
-console.log(applyToEvens([1,2,3,4,5,6,7],n => n*2));
+/*console.log(applyToEvens([1,2,3,4,5,6,7],n => n*2));*/
+/*console.log(productOdds([]));*/
+console.log(strIndexesStr('Hello World',-1,-2));
