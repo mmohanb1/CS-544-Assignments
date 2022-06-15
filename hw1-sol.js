1492eae63de07f5237461b363894fefec3cab0b6
Murali Mohan Bugata <mmohanb1@CS444-mmohanb1.bu.binghamton.edu>
2022-06-15 09:01:05 +0000

    5Functions

--- hw1-sol.js
+++ hw1-sol.js
@@ -13,7 +13,13 @@ function productOdds(list){
 function strIndexesStr(s, ...indexes){
     return Array.from(indexes, a => s[a]).toString().replaceAll(',','');
 }
+function selector(obj, selector){
+    const arr = selector.split('.')
+    const elems = Array.from(arr.slice(1, arr.length)).reduce((acc, e, i) => acc.concat(acc.at(-1)[e]) ,[obj[arr[0]]]);
+    return elems[elems.length-1];
+}
 /*console.log(rmPrefixSufix("twas brillig and the slithy toves",-1,-4));*/
 /*console.log(applyToEvens([1,2,3,4,5,6,7],n => n*2));*/
 /*console.log(productOdds([]));*/
-console.log(strIndexesStr('Hello World',-1,-2));
+//console.log(strIndexesStr('Hello World',-1,-2));
+console.log(selector({a: [1, {b: 22}], c: 33, d: {f: {m: {s: {x: -32}}}}}, 'd.f.m.s.x'));
