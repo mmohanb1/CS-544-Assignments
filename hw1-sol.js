5c74b915bde832b6ae0e71e170554314bd47b8fd
Murali Mohan Bugata <mmohanb1@CS444-mmohanb1.bu.binghamton.edu>
2022-06-16 19:10:22 +0000

    correctedFunction5

--- hw1-sol.js
+++ hw1-sol.js
@@ -14,10 +14,15 @@ function strIndexesStr(s, ...indexes){
 //    return Array.from(indexes, a => s[a]).toString().replaceAll(',','');
     return Array.from(indexes, a => s[a]).join('');
 }
+//function selector(obj, selector){
+  //  const arr = selector.split('.')
+   // const elems = Array.from(arr.slice(1, arr.length)).reduce((acc, e, i) => acc.concat(acc.at(-1)[e]) ,[obj[arr[0]]]);
+//    return elems[elems.length-1];
+//}
 function selector(obj, selector){
     const arr = selector.split('.')
-    const elems = Array.from(arr.slice(1, arr.length)).reduce((acc, e, i) => acc.concat(acc.at(-1)[e]) ,[obj[arr[0]]]);
-    return elems[elems.length-1];
+    return Array.from(arr.slice(1, arr.length)).reduce((acc, e, i) => acc[e] ,obj[arr[0]]);
+    //return elems[elems.length-1];
 }
 function lineAt(str, n)
 {
@@ -68,7 +73,7 @@ const arr2 = arr1.slice(1, arr1.length);
 /*console.log(applyToEvens([1,2,3,4,5,6,7],n => n*2));*/
 /*console.log(productOdds([]));*/
 //console.log(strIndexesStr('Hello World',1,2,6));
-//console.log(selector({a: [1, {b: 22}], c: 33, d: {f: {m: {s: {x: -32}}}}}, 'd.f.m.s.x'));
+console.log(selector({a: [1, {b: 22}], c: 33, d: {f: {m: {s: {x: [-32,'abx',{k: {v: 8}}]}}}}}, 'd.f.m.s.x.2.k.v'));
 //const str = '01\n012\n0123\n01234\n'
 //console.log(fixedLengthLines(str,2));
 //console.log(selector({a: [1, {b: 22}], c: 33}, 'd'));
@@ -76,4 +81,4 @@ const arr2 = arr1.slice(1, arr1.length);
 //console.log(range(4,3));
 //console.log(sumOfPowers(2,8));
 //console.log(iterExponential(3,4));
-console.log(exprExponential(10,2));
+//console.log(exprExponential(10,2));
